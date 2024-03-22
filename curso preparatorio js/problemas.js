75aa31ac3606e50c87a1e40551d7c9257d9bb43d
jessyssttivens <jessymerino_26_1997@hotmail.com>
2024-03-21 22:04:56 -0500

    jessy

---
+++ curso preparatorio js/problemas.js
@@ -0,0 +1,15 @@
+function colors(color) {
+    switch (color) {
+        case "blue":
+          return "this is blue";
+        case "red":
+          return "this is red";
+        case "green":
+          return "this is green";
+        case "orange":
+         return "this is orange";
+        default:
+            return " color not found";
+
+    }
+}
